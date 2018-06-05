f6d859454729fb9d0d076603991ed7c05748949e
ishwar-sukhani <ishwar.sukhani@gmail.com>
2018-06-05 23:13:06 +0530

    Changed Factorial.java to original state

--- SoftEdge/src/home/assignment1/Factorial.java
+++ SoftEdge/src/home/assignment1/Factorial.java
@@ -4,9 +4,9 @@ public class Factorial {
 
 	public static void main(String[] args) {
 		
-		int number1=4, result=1;
+		int number=4, result=1;
 		
-		for(int i=1; i<=number1; i++){
+		for(int i=1; i<=number; i++){
 			result = result * i;
 		}
 		System.out.println("Factorial of "+number+"! is "+result);
