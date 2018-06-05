bfe21b06061acd2f8fdd3a9fe74e92d2683b2992
ishwar-sukhani <ishwar.sukhani@gmail.com>
2018-06-05 22:54:44 +0530

    Changed the name in Factorial.java

--- SoftEdge/src/home/assignment1/Factorial.java
+++ SoftEdge/src/home/assignment1/Factorial.java
@@ -4,9 +4,9 @@ public class Factorial {
 
 	public static void main(String[] args) {
 		
-		int number=4, result=1;
+		int number1=4, result=1;
 		
-		for(int i=1; i<=number; i++){
+		for(int i=1; i<=number1; i++){
 			result = result * i;
 		}
 		System.out.println("Factorial of "+number+"! is "+result);
